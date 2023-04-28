1c6a5f942c2c9f9f5461a07cfee3298c1d4b5fb4
wooyoung0407 <wooyoung0407@naver.com>
2023-04-28 12:22:09 +0900

    SpringBoot-lombok

---
+++ src/main/java/com/example/sbb/HelloLombok.java
@@ -0,0 +1,21 @@
+package com.example.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+    public String hello;
+    private int lombok;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setHello("헬로");
+        helloLombok.setLombok(5);
+
+        System.out.println(helloLombok.getHello());
+        System.out.println(helloLombok.getLombok());
+
+    }
+}
